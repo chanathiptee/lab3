298496f1b56fcbeac5f694b022da43a5ebb55c61
chanathiptee <6531501020@lamduan.mfu.ac.th>
2025-04-20 16:45:03 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ import pickle
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
+st.set_page_config(page_title="K-Means Clustering", layout="centered")
 
 with open('dtm_trained_model.pkl', 'rb') as f :
     load_model = pickle.load(f)
 
 st.title("K-Means Clustering Visualizer by Chanathip Sirisrisermwong")
 
-st.set_page_config(page_title="K-Means Clustering", layout="centered")
 
 X, _ = make_blobs(n_samples=300, centers=load_model.n_clusters, cluster_std=0.60, random_state=0)
 
